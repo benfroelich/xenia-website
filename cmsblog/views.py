48b67e90234321d548c43405ecd8ad07889c514a
Benny <benfroelich@gmail.com>
2022-09-18 20:34:04 -0700

    working version of comment system, needs cleanup and redirection refinements

--- cmsblog/views.py
+++ cmsblog/views.py
@@ -22,27 +22,32 @@ def comment(request, thread_id, post_id):
     # this api uses POST for updates and new comments
     if request.method == "POST":
         try:
-            # check if the thread already exists
-            if thread_id == 'new': # need to create new thread
-                thread = Thread(post_id = post_id)
-                thread.save()
-            else:
-                thread = Thread.objects.get(pk = thread_id)
             # check if we are working with an existing post, meaning
             # either edit (PUT) or deletion (DELETE)
-            if 'comment_pk' in request.POST:
-                comment = Comment.objects.get(pk=request.POST['comment_pk'])
+            if 'comment_id' in request.POST:
+                comment = Comment.objects.get(pk=request.POST['comment_id'])
                 # verify that the owner is the current user
                 if comment.owner == request.user:
                     # handle deletion
                     if request.POST.get('_method', '').lower() == "delete":
-                        comment.delete()
-                        print("delete route")
-                    else # handle edit
+                        # update these here because the form action url uses
+                        # dummy pks
+                        post_id = request.POST.get('post_id', '').lower()
+                        thread_id = request.POST.get('thread_id', '').lower()
+                        print(f'setting post_id to {post_id} and thread_id to {thread_id}')
+                        comment.comment_text = "This comment has been deleted."
+                    else: # handle edit
                         comment.comment_text = request.POST['comment']
-                        comment.last_published_at = datetime.now()
-                        comment.save()
+
+                    comment.last_published_at = datetime.now()
+                    comment.save()
             else:
+                # check if the thread already exists
+                if thread_id == 'new': # need to create new thread
+                    thread = Thread(post_id = post_id)
+                    thread.save()
+                else:
+                    thread = Thread.objects.get(pk = thread_id)
                 # new comment
                 comment = Comment(comment_text = request.POST['comment'], 
                         owner = request.user, thread_id = thread.pk)
